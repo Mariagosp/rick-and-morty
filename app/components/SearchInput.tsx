a726e3c7477a93ed7c9382f988441df73068da79
Maria <m.gosp1704@gmail.com>
2025-03-11 10:33:01 +0200

    fix

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useUpdateSearchParams } from "../utils/updateSearchParams";
 import { Input } from "@chakra-ui/react";
 import { Controller, useForm } from "react-hook-form";
